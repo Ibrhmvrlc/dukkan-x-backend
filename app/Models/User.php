73f14a9a87533c4aac1be4cc967f691736608ee8
Ibrhmvrlc <varelci.i@gmail.com>
2025-04-27 23:45:41 +0300

    Authentication Yapısı
    Bu yapılara işlev kazandırıldı. Artık giriş yapılmamışken bu sayfalar harici herhangi bir sayfaya erişim olmayacak. Giriş yapılmışsa da bu sayfalara ulaşılamayacak.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,12 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+    
     public function getJWTIdentifier()
     {
         return $this->getKey();
